d1de6c687d251fea33b8d58573c777a5c6cf7d53
MrSterdy <mrsterdy@gmail.com>
2023-10-16 18:00:50 +0300

    Update Valute model

--- src/PawPay.Domain/Models/Valute.cs
+++ src/PawPay.Domain/Models/Valute.cs
@@ -1,3 +1,3 @@
 namespace PawPay.Domain.Models;
 
-public record Valute(string Id, string NumCode, string CharCode, string Name, float ValueInRubles);
+public record Valute(string Id, string NumCode, string CharCode, string Name, float Value);
