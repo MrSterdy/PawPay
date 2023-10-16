d599d82ac7ddf5d3985f9a719d54159cc7e9ec19
MrSterdy <mrsterdy@gmail.com>
2023-10-16 18:46:27 +0300

    Add endpoints

--- src/PawPay.Web/Program.cs
+++ src/PawPay.Web/Program.cs
@@ -1,9 +1,31 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PawPay.Application;
+using PawPay.Application.Commands;
+using PawPay.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/convert/rub-to-usd",
+    async ([FromBody] ConvertRublesToDollarsCommand request, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+    {
+        var result = await mediator.Send(request, cancellationToken);
+
+        return result.IsT0 ? Results.Ok(result.AsT0) : Results.BadRequest(result.AsT1);
+    });
+app.MapPost("/convert/usd-to-rub",
+    async ([FromBody] ConvertDollarsToRublesCommand request, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+    {
+        var result = await mediator.Send(request, cancellationToken);
+
+        return result.IsT0 ? Results.Ok(result.AsT0) : Results.BadRequest(result.AsT1);
+    });
 
 app.Run();
